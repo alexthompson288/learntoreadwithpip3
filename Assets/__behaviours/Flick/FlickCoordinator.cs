c613d0f5de9548b8e55150e256b01a5dc2349f46
TomMulvaney <mulvaney.tom@gmail.com>
2014-03-11 18:43:17 +0000

    Updated FlickableWidget. Started making FlickCoordinator.

--- Assets/__behaviours/Flick/FlickCoordinator.cs
+++ Assets/__behaviours/Flick/FlickCoordinator.cs
@@ -1,15 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class FlickCoordinator : MonoBehaviour {
+public class FlickCoordinator : MonoBehaviour 
+{
+	[SerializeField]
+	private GameDataBridge.DataType m_dataType;
+	[SerializeField]
+	private GameObject m_flickablePrefab;
+	[SerializeField]
+	private Transform m_flickableParent;
+	[SerializeField]
+	private AudioSource m_audioSource;
+	[SerializeField]
+	private Vector2 m_spawnDelayRange;
 
-	// Use this for initialization
-	void Start () {
-	
+	List<DataRow> m_dataPool = new List<DataRow>();
+
+	DataRow m_targetData = null;
+
+	IEnumerator Start()
+	{
+		// Clamp spawn delay range values
+		m_spawnDelayRange.x = Mathf.Clamp (m_spawnDelayRange.x, 0, m_spawnDelayRange.x);
+		m_spawnDelayRange.y = Mathf.Clamp (m_spawnDelayRange.y, m_spawnDelayRange.x, m_spawnDelayRange.y);
+
+		yield return StartCoroutine (GameDataBridge.WaitForDatabase());
+
+		switch (m_dataType) 
+		{
+		case GameDataBridge.DataType.Letters:
+			m_dataPool = GameDataBridge.Instance.GetLetters();
+			break;
+		case GameDataBridge.DataType.Keywords:
+			m_dataPool = GameDataBridge.Instance.GetKeywords();
+			break;
+		case GameDataBridge.DataType.Words:
+			m_dataPool = GameDataBridge.Instance.GetWords();
+			break;
+		}
+
+		StartCoroutine (SpawnFlickables ());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	IEnumerator SpawnFlickables()
+	{
+		yield return new WaitForSeconds (Random.Range (m_spawnDelayRange.x, m_spawnDelayRange.y));
+	}
+
+	void NewTarget()
+	{
+		m_targetData = m_dataPool [Random.Range (0, m_dataPool.Count)];
+		SayTarget ();
+	}
+
+	void SayTarget()
+	{
+		if (m_targetData != null) 
+		{
+			AudioClip clip = m_dataType == GameDataBridge.DataType.Letters ?
+				AudioBankManager.Instance.GetAudioClip(m_targetData["grapheme"].ToString()) :
+					LoaderHelpers.LoadAudioForWord (m_targetData["word"].ToString());
+
+			if (clip != null) 
+			{
+				m_audioSource.clip = clip;
+				m_audioSource.Play();
+			}
+		}
 	}
 }
--- Assets/__behaviours/Flick/FlickableWidget.cs
+++ Assets/__behaviours/Flick/FlickableWidget.cs
@@ -6,7 +6,7 @@ public class FlickableWidget : MonoBehaviour
 	[SerializeField]
 	private Vector3 m_gravity = new Vector3(0, -0.5f, 0);
 	[SerializeField]
-	private float m_maxFallSpeed = -2f;
+	private Vector3 m_maxSpeed = new Vector3 (2, 2, 0);
 
 	Vector3 m_dragOffset;
 
@@ -16,9 +16,19 @@ public class FlickableWidget : MonoBehaviour
 
 	bool m_applyFlick = false;
 
+	DataRow m_data = null;
+
 	void Awake()
 	{
+		m_maxSpeed.x = Mathf.Abs (m_maxSpeed.x);
+		m_maxSpeed.y = Mathf.Abs (m_maxSpeed.y);
+		m_maxSpeed.z = 0;
 		m_gravity.z = 0;
+	}
+
+	public void SetUp(DataRow data)
+	{
+		m_data = data;
 	}
 
 	void FixedUpdate()
@@ -27,19 +37,31 @@ public class FlickableWidget : MonoBehaviour
 		{
 			rigidbody.AddForce(m_gravity);
 
-			if(rigidbody.velocity.y < m_maxFallSpeed)
-			{
-				Vector3 newVelocity = rigidbody.velocity;
-				newVelocity.y = m_maxFallSpeed;
-				rigidbody.velocity = newVelocity;
-			}
-
 			if(m_applyFlick)
 			{
-				Vector3 velocity = m_lastPos - transform.position;
-				rigidbody.velocity = velocity;
+				Vector3 flickVelocity = (transform.position - m_lastPos) / Time.deltaTime;
+				Debug.Log("Apply: " + flickVelocity);
+				rigidbody.velocity = flickVelocity;
 				m_applyFlick = false;
 			}
+
+			Vector3 velocity = rigidbody.velocity;
+			
+			if(Mathf.Abs(velocity.y) > m_maxSpeed.y)
+			{
+				velocity.y = velocity.y / Mathf.Abs(velocity.y) * m_maxSpeed.y; // Set velocity.y to equal m_maxSpeed.y but keep its sign
+			}
+			
+			if(Mathf.Abs(velocity.x) > m_maxSpeed.x)
+			{
+				velocity.x = velocity.x / Mathf.Abs(velocity.x) * m_maxSpeed.x; // Set velocity.x to equal m_maxSpeed.x but keep its sign
+			}
+			
+			rigidbody.velocity = velocity;
+		}
+		else
+		{
+			rigidbody.velocity = Vector3.zero;
 		}
 
 		m_lastPos = transform.position;
@@ -51,6 +73,8 @@ public class FlickableWidget : MonoBehaviour
 
 		if (m_pressed)
 		{
+			rigidbody.velocity = Vector3.zero;
+
 			iTween.Stop(gameObject);
 				
 			Ray camPos = UICamera.currentCamera.ScreenPointToRay(new Vector3(UICamera.currentTouch.pos.x, UICamera.currentTouch.pos.y, 0));
@@ -69,5 +93,11 @@ public class FlickableWidget : MonoBehaviour
 		transform.position = new Vector3(camPos.origin.x, camPos.origin.y, 0) - m_dragOffset;
 			
 		m_dragOffset = m_dragOffset - (Time.deltaTime * m_dragOffset);
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label ("Velocity: " + rigidbody.velocity);
+		GUILayout.Label ("Speed: " + rigidbody.velocity.magnitude);
 	}
 }
