8838cb1275ad27ec8de7412d0cd90bfcbc7fca71
TomMulvaney <mulvaney.tom@gmail.com>
2014-03-17 19:06:32 +0000

    Updated CannonBehaviour and CannonBall. Created CannonBall prefab.

--- Assets/__behaviours/Cannon/CannonBall.cs
+++ Assets/__behaviours/Cannon/CannonBall.cs
@@ -1,19 +1,126 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Wingrove;
 
 public class CannonBall : MonoBehaviour 
 {
+	[SerializeField]
+	private string m_pressedAudio = "BUTTON_PRESS";
+	[SerializeField]
+	private string m_unpressedAudio = "BUTTON_UNPRESS";
+	[SerializeField]
+	private Vector3 m_gravity = new Vector3(0, -0.5f, 0);
+	[SerializeField]
+	private bool m_limitSpeed;
+	[SerializeField]
+	private Vector3 m_maxSpeed = new Vector3 (2, 2, 0);
+
+#if UNITY_EDITOR
+	[SerializeField]
+	UISprite m_sprite;
+#endif
+
+	bool m_useGravity = false;
+
 	DataRow m_data;
+
+	Vector3 m_dragOffset;
+
+	bool m_canDrag = true;
 
 	void Start()
 	{
 		//iTween.ScaleFrom (gameObject, Vector3.zero, 0.5f); // This may or may not interfere with rigidbody motion, test it out first
 	}
 
+#if UNITY_EDITOR
+	void Update()
+	{
+		Vector3 cannonDelta = transform.position - CannonBehaviour.Instance.GetBallCentrePos();
+		if (cannonDelta.magnitude > CannonBehaviour.Instance.GetMaxPull ()) 
+		{
+			m_sprite.color = Color.red;
+		} 
+		else if (cannonDelta.magnitude < CannonBehaviour.Instance.GetMinPull ()) 
+		{
+			m_sprite.color = Color.black;
+		} 
+		else 
+		{
+			m_sprite.color = Color.white;
+		}
+	}
+#endif
+
 	public void SetUp(DataRow data)
 	{
 		m_data = data;
 	}
 
+	public void SetCanDrag(bool canDrag)
+	{
+		m_canDrag = canDrag;
+	}
 
+	void FixedUpdate()
+	{
+		if (m_useGravity) 
+		{
+			rigidbody.AddForce(m_gravity);
+		}
+
+		if(m_limitSpeed)
+		{
+			Vector3 velocity = rigidbody.velocity;
+			
+			if(Mathf.Abs(velocity.y) > m_maxSpeed.y)
+			{
+				velocity.y = velocity.y / Mathf.Abs(velocity.y) * m_maxSpeed.y; // Set velocity.y to equal m_maxSpeed.y but keep its sign
+			}
+			
+			if(Mathf.Abs(velocity.x) > m_maxSpeed.x)
+			{
+				velocity.x = velocity.x / Mathf.Abs(velocity.x) * m_maxSpeed.x; // Set velocity.x to equal m_maxSpeed.x but keep its sign
+			}
+			
+			rigidbody.velocity = velocity;
+		}
+	}
+
+	void OnPress(bool press)
+	{
+		WingroveAudio.WingroveRoot.Instance.PostEvent (press ? m_pressedAudio : m_unpressedAudio);
+		
+		if(m_canDrag)
+		{
+			if (press)
+			{
+				iTween.Stop(gameObject);
+
+				Ray camPos = UICamera.currentCamera.ScreenPointToRay(new Vector3(UICamera.currentTouch.pos.x, UICamera.currentTouch.pos.y, 0));
+				m_dragOffset = new Vector3(camPos.origin.x, camPos.origin.y, 0) - transform.position;
+			}
+			else
+			{
+				CannonBehaviour.Instance.OnBallRelease(rigidbody);
+			}
+		}
+	}
+
+	void OnDrag(Vector2 dragAmount)
+	{
+		if(m_canDrag)
+		{
+			Ray camPos = UICamera.currentCamera.ScreenPointToRay(new Vector3(UICamera.currentTouch.pos.x, UICamera.currentTouch.pos.y, 0));
+			transform.position = new Vector3(camPos.origin.x, camPos.origin.y, 0) - m_dragOffset;
+			
+			m_dragOffset = m_dragOffset - (Time.deltaTime * m_dragOffset);
+
+			Vector3 cannonDelta = transform.position - CannonBehaviour.Instance.GetBallCentrePos();
+			if(cannonDelta.magnitude > CannonBehaviour.Instance.GetMaxPull())
+			{
+				transform.position = CannonBehaviour.Instance.GetBallCentrePos() + (cannonDelta.normalized * CannonBehaviour.Instance.GetMaxPull());
+			}
+		}
+	}
 }
--- Assets/__behaviours/Cannon/CannonBehaviour.cs
+++ Assets/__behaviours/Cannon/CannonBehaviour.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Wingrove;
 
 public class CannonBehaviour : Singleton<CannonBehaviour> 
 {
 	[SerializeField]
 	private GameObject m_cannonBallPrefab;
 	[SerializeField]
-	private Transform m_ballRestLocation;
+	private Transform m_ballCentre;
 	[SerializeField]
 	private Vector2 m_pullRange;
 	[SerializeField]
 	private Vector2 m_forceRange;
+
+#if UNITY_EDITOR
+	[SerializeField]
+	private bool m_instantiateDebugBall;
+#endif
 
 	List<CannonBall> m_spawnedBalls = new List<CannonBall>();
 
@@ -19,11 +25,18 @@ public class CannonBehaviour : Singleton<CannonBehaviour>
 	{
 		m_pullRange.x = Mathf.Clamp (m_pullRange.x, 0, m_pullRange.x);
 		m_forceRange.x = Mathf.Clamp (m_forceRange.x, 0, m_forceRange.x);
+
+#if UNITY_EDITOR
+		if(m_instantiateDebugBall)
+		{
+			SpawningHelpers.InstantiateUnderWithIdentityTransforms(m_cannonBallPrefab, m_ballCentre);
+		}
+#endif
 	}
 
-	public Vector3 GetBallRestPosition()
+	public Vector3 GetBallCentrePos()
 	{
-		return m_ballRestLocation.position;
+		return m_ballCentre.position;
 	}
 
 	public float GetMaxPull()
@@ -31,11 +44,16 @@ public class CannonBehaviour : Singleton<CannonBehaviour>
 		return m_pullRange.y;
 	}
 
+	public float GetMinPull()
+	{
+		return m_pullRange.x;
+	}
+
 	public void OnBallRelease(Rigidbody rb)
 	{
-		if ((m_ballRestLocation.position - rb.transform.position).magnitude < m_pullRange.x) 
+		if ((m_ballCentre.position - rb.transform.position).magnitude < m_pullRange.x) 
 		{
-			iTween.MoveTo (rb.gameObject, m_ballRestLocation.position, 0.3f);
+			iTween.MoveTo (rb.gameObject, m_ballCentre.position, 0.3f);
 		} 
 		else 
 		{
@@ -47,7 +65,7 @@ public class CannonBehaviour : Singleton<CannonBehaviour>
 	{
 		Debug.Log ("CannonBehaviour.Launch()");
 
-		Vector3 delta = m_ballRestLocation.position - rb.transform.position;
+		Vector3 delta = m_ballCentre.position - rb.transform.position;
 
 		float distance = delta.magnitude;
 		float proportionalDistance = (distance - m_pullRange.x) / (m_pullRange.y - m_pullRange.x);
