0ef09a70d14ab4a741b70a8ad4ef5d58a57e84db
TomMulvaney <mulvaney.tom@gmail.com>
2014-05-07 15:44:20 +0100

    Deleted EyeSpy scenes, scripts and prefabs. Deleted ChooseCorrectPath scenes, scripts, prefabs and art.

--- Assets/__behaviours/Enviro/CorrectPathEnviro.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CorrectPathEnviro : MonoBehaviour 
-{
-	public Texture2D[] m_backgrounds;
-	public Texture2D m_finalBackground;
-	public Texture2D m_platform;
-}
