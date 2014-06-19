4b6ab6491519f9842998f231a065f06330b0704c
TomMulvaney <mulvaney.tom@gmail.com>
2014-06-19 19:04:34 +0100

    Moved AnimManager to UI directory. Created SpriteAnim (currently has all functionality of SimpleSpriteAnim but refactored to be cleaner and more efficient.

--- Assets/__behaviours/UI/SimpleSpriteAnim.cs
+++ Assets/__behaviours/UI/SimpleSpriteAnim.cs
@@ -1,139 +1,4 @@
-﻿/*
-using UnityEngine;
-using System.Collections;
-
-public class SimpleSpriteAnim : MonoBehaviour 
-{
-	public delegate void EventHandler(string animName);
-	public event EventHandler AnimFinished;
-	
-	[System.Serializable]
-	public class AnimDetails
-	{
-		[SerializeField]
-		public string m_name;
-		[SerializeField]
-		public string m_prefix;
-		[SerializeField]
-		public int m_numFrames;
-		[SerializeField]
-		public int m_lowFrame = 0;
-		[SerializeField]
-		public bool m_toLetter = false;
-		[SerializeField]
-		public float m_fps;
-		[SerializeField]
-		public bool m_oneBased;
-		[SerializeField]
-		public int m_startFrame = 1;
-		[SerializeField]
-		public bool m_loop = true;
-	}
-	
-	[SerializeField]
-	AnimDetails[] m_availableAnimations;
-	[SerializeField]
-	UISprite m_sprite;
-	[SerializeField]
-	string m_format = "{0}";
-	
-	int m_currentAnimation;
-	float m_currentT;
-	int m_currentFrame;
-	
-	bool m_sentEvent;
-	
-	public void PlayAnimation(string animName)
-	{
-		m_sentEvent = false;
-		int index = 0;
-		foreach (AnimDetails ad in m_availableAnimations)
-		{
-			if (ad.m_name == animName)
-			{
-				m_currentAnimation = index;
-				m_currentFrame = 0;
-			}
-			++index;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		if (string.IsNullOrEmpty(m_format))
-		{
-			m_format = "{0}";
-		}
-		if (m_availableAnimations.Length > m_currentAnimation)
-		{
-			m_currentT += Time.deltaTime * m_availableAnimations[m_currentAnimation].m_fps;
-			if (m_currentT > 1)
-			{
-				m_currentT -= 1;
-				m_currentFrame++;
-				
-				//if (m_currentFrame >= m_availableAnimations[m_currentAnimation].m_numFrames)
-				if (m_currentFrame >= m_availableAnimations[m_currentAnimation].m_numFrames)
-				{
-					if(m_availableAnimations[m_currentAnimation].m_loop)
-					{
-						m_currentFrame = 0;
-					}
-					else if(!m_sentEvent)
-					{
-						if(AnimFinished != null)
-						{
-							AnimFinished(m_availableAnimations[m_currentAnimation].m_name);
-						}
-						
-						m_sentEvent = true;
-					}
-				}
-				
-				if(m_currentFrame < m_availableAnimations[m_currentAnimation].m_numFrames)
-				{
-					int frame = m_currentFrame + m_availableAnimations[m_currentAnimation].m_lowFrame;
-					frame += m_availableAnimations[m_currentAnimation].m_startFrame;
-
-					string frameString = string.Format(m_format, frame);
-					if (m_availableAnimations[m_currentAnimation].m_toLetter)
-					{
-						frameString = ((char)('a' + frame)).ToString();
-					}
-					m_sprite.spriteName = m_availableAnimations[m_currentAnimation].m_prefix + frameString;
-				}
-			}
-		}
-	}
-	
-	public string[] GetAnimNames()
-	{
-		string[] animNames = new string[m_availableAnimations.Length];
-		for(int i = 0; i < m_availableAnimations.Length; ++i)
-		{
-			animNames[i] = m_availableAnimations[i].m_name;
-		}
-		
-		return animNames;
-	}
-	
-	public void SetSprite(UISprite sprite)
-	{
-		m_sprite = sprite;
-	}
-	
-	public void SetAnimFPS(int newFPS)
-	{
-		foreach(AnimDetails animDetail in m_availableAnimations)
-		{
-			animDetail.m_fps = newFPS;
-		}
-	}
-}
-*/
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class SimpleSpriteAnim : MonoBehaviour 
@@ -222,15 +87,10 @@ public class SimpleSpriteAnim : MonoBehaviour
                 m_currentT -= 1;
                 m_currentFrame++;
 
-                //if (m_currentFrame >= m_availableAnimations[m_currentAnimation].m_numFrames)
 				if (m_currentFrame >= m_availableAnimations[m_currentAnimation].m_numFrames)
 				{
 					if(m_availableAnimations[m_currentAnimation].m_loop)
 					{
-                        if(transform.parent.name.Contains("FlowerBlue") && m_availableAnimations[m_currentAnimation].m_name == "ON")
-                        {
-                            Debug.Log("Looping ON");
-                        }
                     	m_currentFrame = 0;
 					}
 					else if(!m_sentEvent)
@@ -308,3 +168,138 @@ public class SimpleSpriteAnim : MonoBehaviour
         return foundAnim;
     }
 }
+
+/*
+using UnityEngine;
+using System.Collections;
+
+public class SimpleSpriteAnim : MonoBehaviour 
+{
+    public delegate void EventHandler(string animName);
+    public event EventHandler AnimFinished;
+    
+    [System.Serializable]
+    public class AnimDetails
+    {
+        [SerializeField]
+        public string m_name;
+        [SerializeField]
+        public string m_prefix;
+        [SerializeField]
+        public int m_numFrames;
+        [SerializeField]
+        public int m_lowFrame = 0;
+        [SerializeField]
+        public bool m_toLetter = false;
+        [SerializeField]
+        public float m_fps;
+        [SerializeField]
+        public bool m_oneBased;
+        [SerializeField]
+        public int m_startFrame = 1;
+        [SerializeField]
+        public bool m_loop = true;
+    }
+    
+    [SerializeField]
+    AnimDetails[] m_availableAnimations;
+    [SerializeField]
+    UISprite m_sprite;
+    [SerializeField]
+    string m_format = "{0}";
+    
+    int m_currentAnimation;
+    float m_currentT;
+    int m_currentFrame;
+    
+    bool m_sentEvent;
+    
+    public void PlayAnimation(string animName)
+    {
+        m_sentEvent = false;
+        int index = 0;
+        foreach (AnimDetails ad in m_availableAnimations)
+        {
+            if (ad.m_name == animName)
+            {
+                m_currentAnimation = index;
+                m_currentFrame = 0;
+            }
+            ++index;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () 
+    {
+        if (string.IsNullOrEmpty(m_format))
+        {
+            m_format = "{0}";
+        }
+        if (m_availableAnimations.Length > m_currentAnimation)
+        {
+            m_currentT += Time.deltaTime * m_availableAnimations[m_currentAnimation].m_fps;
+            if (m_currentT > 1)
+            {
+                m_currentT -= 1;
+                m_currentFrame++;
+                
+                //if (m_currentFrame >= m_availableAnimations[m_currentAnimation].m_numFrames)
+                if (m_currentFrame >= m_availableAnimations[m_currentAnimation].m_numFrames)
+                {
+                    if(m_availableAnimations[m_currentAnimation].m_loop)
+                    {
+                        m_currentFrame = 0;
+                    }
+                    else if(!m_sentEvent)
+                    {
+                        if(AnimFinished != null)
+                        {
+                            AnimFinished(m_availableAnimations[m_currentAnimation].m_name);
+                        }
+                        
+                        m_sentEvent = true;
+                    }
+                }
+                
+                if(m_currentFrame < m_availableAnimations[m_currentAnimation].m_numFrames)
+                {
+                    int frame = m_currentFrame + m_availableAnimations[m_currentAnimation].m_lowFrame;
+                    frame += m_availableAnimations[m_currentAnimation].m_startFrame;
+
+                    string frameString = string.Format(m_format, frame);
+                    if (m_availableAnimations[m_currentAnimation].m_toLetter)
+                    {
+                        frameString = ((char)('a' + frame)).ToString();
+                    }
+                    m_sprite.spriteName = m_availableAnimations[m_currentAnimation].m_prefix + frameString;
+                }
+            }
+        }
+    }
+    
+    public string[] GetAnimNames()
+    {
+        string[] animNames = new string[m_availableAnimations.Length];
+        for(int i = 0; i < m_availableAnimations.Length; ++i)
+        {
+            animNames[i] = m_availableAnimations[i].m_name;
+        }
+        
+        return animNames;
+    }
+    
+    public void SetSprite(UISprite sprite)
+    {
+        m_sprite = sprite;
+    }
+    
+    public void SetAnimFPS(int newFPS)
+    {
+        foreach(AnimDetails animDetail in m_availableAnimations)
+        {
+            animDetail.m_fps = newFPS;
+        }
+    }
+}
+*/
---
+++ Assets/__behaviours/UI/SpriteAnim.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnim : MonoBehaviour 
+{
+    public delegate void AnimFinishedEventHandler(SpriteAnim animBehaviour, string animName);
+    public event AnimFinishedEventHandler AnimFinished;
+    
+    [System.Serializable]
+    public class AnimDetails
+    {
+        [SerializeField]
+        public UIAtlas m_spriteAtlas;
+        [SerializeField]
+        public string m_name;
+        [SerializeField]
+        public string m_prefix;
+        [SerializeField]
+        public int m_numFrames;
+        [SerializeField]
+        public int m_startFrame = 1;
+        [SerializeField]
+        public bool m_toLetter = false;
+        [SerializeField]
+        public float m_fps;
+        [SerializeField]
+        public bool m_loop = true;
+    }
+
+    [SerializeField]
+    bool m_playAnim = true;
+    [SerializeField]
+    AnimDetails[] m_availableAnimations;
+    [SerializeField]
+    UISprite m_sprite;
+    [SerializeField]
+    string m_format = "{0}";
+    
+    int m_currentAnimation;
+    float m_currentT;
+    int m_currentFrame;
+    
+    void Awake()
+    {
+        if(m_sprite == null)
+        {
+            m_sprite = GetComponent<UISprite>() as UISprite;
+        }
+
+        if (string.IsNullOrEmpty(m_format))
+        {
+            m_format = "{0}";
+        }
+    }
+    
+    public void PlayAnimation(string animName)
+    {
+        int index = 0;
+        foreach (AnimDetails ad in m_availableAnimations)
+        {
+            if (ad.m_name == animName)
+            {
+                m_currentAnimation = index;
+                m_currentFrame = 0;
+                
+                if(ad.m_spriteAtlas != null)
+                {
+                    m_sprite.atlas = ad.m_spriteAtlas;
+                }
+                
+                SetSpriteName();
+
+                m_playAnim = true;
+            }
+            ++index;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        if (m_playAnim && m_currentAnimation < m_availableAnimations.Length)
+        {
+            m_currentT += Time.deltaTime * m_availableAnimations[m_currentAnimation].m_fps;
+            if (m_currentT > 1)
+            {
+                m_currentT -= 1;
+                m_currentFrame++;
+
+                if(m_currentFrame < m_availableAnimations[m_currentAnimation].m_numFrames)
+                {
+                    SetSpriteName();
+                }
+                else
+                {
+                    if(m_availableAnimations[m_currentAnimation].m_loop)
+                    {
+                        m_currentFrame = 0;
+                    }
+                    else
+                    {
+                        if(AnimFinished != null)
+                        {
+                            AnimFinished(this, m_availableAnimations[m_currentAnimation].m_name);
+                        }
+
+                        m_playAnim = false;
+                    }
+                }                
+            }
+        }
+    }
+    
+    void SetSpriteName()
+    {
+        int frame = m_currentFrame + m_availableAnimations[m_currentAnimation].m_startFrame;
+        string frameString = string.Format(m_format, frame);
+
+        if (m_availableAnimations[m_currentAnimation].m_toLetter)
+        {
+            frameString = ((char)('a' + frame)).ToString();
+        }
+        
+        m_sprite.spriteName = m_availableAnimations [m_currentAnimation].m_prefix + frameString;
+    }
+    
+    public string[] GetAnimNames()
+    {
+        string[] animNames = new string[m_availableAnimations.Length];
+        for(int i = 0; i < m_availableAnimations.Length; ++i)
+        {
+            animNames[i] = m_availableAnimations[i].m_name;
+        }
+        
+        return animNames;
+    }
+    
+    public void SetSprite(UISprite sprite)
+    {
+        m_sprite = sprite;
+    }
+    
+    public void SetAnimFPS(int newFPS)
+    {
+        foreach(AnimDetails animDetail in m_availableAnimations)
+        {
+            animDetail.m_fps = newFPS;
+        }
+    }
+    
+    public bool HasAnim(string animName)
+    {
+        bool foundAnim = false;
+        
+        foreach (AnimDetails anim in m_availableAnimations)
+        {
+            if(anim.m_name == animName)
+            {
+                foundAnim = true;
+                break;
+            }
+        }
+        
+        return foundAnim;
+    }
+}
