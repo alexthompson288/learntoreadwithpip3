a002bf06e50a57bf800b85bbfb4099c57d170f9d
TomMulvaney <mulvaney.tom@gmail.com>
2014-07-15 14:04:18 +0100

    Renamed button art. Added art to ButtonAtlas. Deleted deprecated art. Updated visuals in NewStoryMenu and NewMainMenu. Updated visuals on NavMenuReading and NavMenuMaths prefabs.

---
+++ Assets/__system/ChangeProgramme.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeProgramme : MonoBehaviour 
+{
+	[SerializeField]
+	private string m_programmeName;
+
+	void OnClick()
+	{
+		GameManager.Instance.SetProgramme(m_programmeName);
+	}
+}
