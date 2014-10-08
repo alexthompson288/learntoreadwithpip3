14a0c66c765eb358612f5af5b70980cba2ee3756
TomMulvaney <mulvaney.tom@gmail.com>
2014-10-08 18:11:52 +0100

    Created IPChecker. Updated PipAnalytics

--- Assets/__behaviours/Analytics/PipAnalytics.cs
+++ Assets/__behaviours/Analytics/PipAnalytics.cs
@@ -6,9 +6,35 @@ public class PipAnalytics : Singleton<PipAnalytics>
     [SerializeField]
     private GoogleAnalyticsV3 m_analytics;
 
+    float m_startTime;
+
+    IEnumerator Start()
+    {
+        yield return StartCoroutine(IPChecker.WaitForResolution());
+        Launched();
+    }
+
+    // TODO: Call Launched after the app is started after Pausing
+
+
     public void Launched()
     {
+        m_startTime = Time.time;
 
+#if UNITY_STANDALONE_OSX
+        string deviceType = "OSX";
+#elif UNITY_STANDALONE
+        string deviceType = "Windows";
+#else
+        string deviceType = "iOS";
+#endif
+
+        deviceType += SystemInfo.deviceModel;
+
+        m_analytics.LogEvent(new EventHitBuilder()
+                             .SetEventCategory("Launched")
+                             .SetEventAction(deviceType)
+                             .SetEventLabel(IPChecker.Instance.ipAddress));
     }
 
     public void ActivitiesCompleted()
@@ -16,13 +42,7 @@ public class PipAnalytics : Singleton<PipAnalytics>
         m_analytics.LogEvent(new EventHitBuilder()
                              .SetEventCategory("ActivitiesCompleted")
                              .SetEventAction(GameManager.Instance.activity)
-                             .SetEventLabel(string.Format("{0}__{1}__{2}", GameManager.Instance.pipColor.ToString(), GameManager.Instance.gameName, SessionInformation.Instance.GetNumPlayers())));
-
-//        googleAnalytics.LogEvent(new EventHitBuilder()
-//                                 .SetEventCategory("Achievement")
-//                                 .SetEventAction("Unlocked")
-//                                 .SetEventLabel("Slay 10 dragons")
-//                                 .SetEventValue(5));
+                             .SetEventLabel(string.Format("{0}__{1}__{2}", GameManager.Instance.pipColor.ToString(), GameManager.Instance.gameName, SessionInformation.Instance.GetNumPlayers())));                                 .SetEventValue(5));
     }
 
     public void ActivitiesCancelled()
@@ -31,5 +51,21 @@ public class PipAnalytics : Singleton<PipAnalytics>
                              .SetEventCategory("ActivitiesCancelled")
                              .SetEventAction(GameManager.Instance.activity)
                              .SetEventLabel(string.Format("{0}__{1}", GameManager.Instance.pipColor.ToString(), GameManager.Instance.gameName)));
+    }
+
+    void OnApplicationQuit()
+    {
+        Exiting();
+    }
+
+    void OnApplicationPause()
+    {
+        Exiting();
+    }
+
+    // TODO
+    void Exiting()
+    {
+        float m_time = Time.time - m_startTime;
     }
 }
---
+++ Assets/__system/IPChecker.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IPChecker : Singleton<IPChecker> 
+{
+    string m_ipAddress = "";
+    public string ipAddress
+    {
+        get
+        {
+            return m_ipAddress;
+        }
+    }
+
+    public static IEnumerator WaitForResolution()
+    {
+        while (Instance == null)
+        {
+            yield return null;
+        }
+
+        while (string.IsNullOrEmpty(Instance.m_ipAddress))
+        {
+            yield return null;
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(FindIpAddress());
+    }
+
+    IEnumerator FindIpAddress()
+    {   
+        WWW myExtIPWWW = null;
+        
+        try
+        {
+            myExtIPWWW = new WWW("http://checkip.dyndns.org");
+        }
+        catch
+        {
+            myExtIPWWW = null;
+        }
+        
+        if (myExtIPWWW != null)
+        {
+            while (!myExtIPWWW.isDone)
+            {
+                yield return null;
+            }
+            
+            try
+            {
+                m_ipAddress = myExtIPWWW.data;
+                m_ipAddress = m_ipAddress.Substring(m_ipAddress.IndexOf(":") + 1);
+                m_ipAddress = m_ipAddress.Substring(0, m_ipAddress.IndexOf("<"));
+            }
+            catch
+            {
+                m_ipAddress = "No_IP_Found";
+            }
+        }         
+    }
+}
