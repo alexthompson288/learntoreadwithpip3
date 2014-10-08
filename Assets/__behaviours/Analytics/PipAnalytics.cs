c5bae7117c1c0db8e841f51aa745e408c6a879cf
TomMulvaney <mulvaney.tom@gmail.com>
2014-10-08 18:14:12 +0100

    Fixed error in PipAnalytics

--- Assets/__behaviours/Analytics/PipAnalytics.cs
+++ Assets/__behaviours/Analytics/PipAnalytics.cs
@@ -42,7 +42,7 @@ public class PipAnalytics : Singleton<PipAnalytics>
         m_analytics.LogEvent(new EventHitBuilder()
                              .SetEventCategory("ActivitiesCompleted")
                              .SetEventAction(GameManager.Instance.activity)
-                             .SetEventLabel(string.Format("{0}__{1}__{2}", GameManager.Instance.pipColor.ToString(), GameManager.Instance.gameName, SessionInformation.Instance.GetNumPlayers())));                                 .SetEventValue(5));
+                             .SetEventLabel(string.Format("{0}__{1}__{2}", GameManager.Instance.pipColor.ToString(), GameManager.Instance.gameName, SessionInformation.Instance.GetNumPlayers())));
     }
 
     public void ActivitiesCancelled()
